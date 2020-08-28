d1d8810c77a81b30f3fc2b6aee3ff33911575f3b
YaoChing <eugeneyubni@gmail.com>
2020-08-28 18:08:08 +0800

    前端頁面產生

---
+++ detail.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>問題對話頁</title>
+  </head>
+  <body>
+    問題對話頁
+  </body>
+</html>
