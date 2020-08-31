403282285f5e8ecf75103c6cb1bc7f30e727c18c
YaoChing <eugeneyubni@gmail.com>
2020-08-31 10:25:14 +0800

    新增登入頁面

--- src/Login/views/main.tsx
+++ src/Login/views/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default () => {
+export default (props: any) => {
   return (
-    <div>
-      會員登入頁
-    </div>
+    <form className="form-signin">
+      <h1 className="h3 mb-3 font-weight-normal">QA管理系統</h1>
+      <input type="email" className="form-control" placeholder="登入帳號" />
+      <input type="password" className="form-control" placeholder="登入密碼" />
+      <button type="button" className="btn btn-primary btn-lg btn-block">登入</button>
+      <button type="button" className="btn btn-danger btn-lg btn-block">取消</button>
+    </form>
   );
 }
