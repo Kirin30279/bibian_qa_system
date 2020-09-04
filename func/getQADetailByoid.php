5c2586a758516cb2ef66ac7a1bb74b9ab889a26b
DESKTOP-KLJVRMO\bibianuser <andy860826@japan-import.com.tw>
2020-09-04 15:06:03 +0800

    getQADetail SQL只查詢一次

--- func/getQADetailByoid.php
+++ func/getQADetailByoid.php
@@ -9,48 +9,40 @@ function getQADetailByoid($oid,$connect){
         return $result;
     }
 
-    $QA_list_SQL =
-    "SELECT isSend,status FROM `qa_list` WHERE `oid`=$oid";//讀取回答狀態、送信狀態
-    $resultQA_list = $connect->query($QA_list_SQL);
-
-    $QA_SQL = 
-    "SELECT user_id,time,oid_Question,PicFile FROM `customer_qa` WHERE `oid`=$oid 
-    ORDER BY `time` ASC";//讀取問答內容
-    $resultQA = $connect->query($QA_SQL);
-
-    $rows = $resultQA->num_rows;
+    $query =   "SELECT list.isSend, list.status,
+    qa.user_id, qa.time, qa.oid_Question, qa.PicFile
+    FROM qa_list as list, customer_qa as qa
+    WHERE list.oid=$oid AND qa.oid=$oid ";
+    
+    $SQL_query = $connect->query($query);
+    $rows = $SQL_query->num_rows;
     if ($rows==0){
         $message = "訂單編號不存在，請重新輸入。";
         $result['message'] = $message;
         return $result;
-
     }
-    $dataArray = array();
 
-    $QAlistData = $resultQA_list->fetch_array(MYSQLI_ASSOC);//QAlist存回答狀態、寫信與否
-    $dataArray['isSend'] = $QAlistData['isSend'];
-    $dataArray['status'] = $QAlistData['status'];
-
-    $QAContentData  = $resultQA->fetch_all(MYSQLI_ASSOC); 
-
-    $QAContentData = array_map(function($QAContentData) {
+    $SQL_result = $SQL_query->fetch_all(MYSQLI_ASSOC); 
+    $QAContentData = array_map(function($SQL_result) {
         return array(
-            'name'        => $QAContentData['user_id'],
-            'createdTime' => $QAContentData['time'],
-            'contents'    => $QAContentData['oid_Question'],
-            'files'       => $QAContentData['PicFile']
+            'name'        => $SQL_result['user_id'],
+            'createdTime' => $SQL_result['time'],
+            'contents'    => $SQL_result['oid_Question'],
+            'files'       => $SQL_result['PicFile']
         );
-    }, $QAContentData);
-        
-    $dataArray['items'] = $QAContentData;
+    }, $SQL_result);
+  
     $result = array(
         'status' => true,
         'message'=> '',
-        'data'   => $dataArray
+        'data'   => array(
+            'isSend' => $SQL_result[0]['isSend'],
+            'stasus' => $SQL_result[0]['status'],
+            'items'  => $QAContentData
+        )
     );
-    
-
     return $result;
+
 }
 
 
