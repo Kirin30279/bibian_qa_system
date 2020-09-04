f17169d59cd9e99b6515c3c4906d9d6a743a32af
DESKTOP-KLJVRMO\bibianuser <andy860826@japan-import.com.tw>
2020-09-04 15:39:58 +0800

    把多個SQL指令整合在一起，一次送出query

--- func/writeQACustomerContent.php
+++ func/writeQACustomerContent.php
@@ -18,25 +18,16 @@ function writeQACustomerContent($login_token,$oid,$qa_content,$qa_files,$connect
         $filepath = 'upload_test/' .  $newFileName;
         move_uploaded_file($file, $filepath); 
     }
- 
-
 
     $insertSQL_Customer_qa = 
     "INSERT INTO customer_qa(oid,oid_Question,PicFile,user_id,time,IsCustomer)
-    VALUES ($oid,'$qa_content','$filepath','$user_name','$date',1)";
-    mysqli_query($connect, $insertSQL_Customer_qa);
-
-    //第二筆上傳，方便後台列表顯示
-    $insertSQL_QA_list = 
-    "INSERT INTO qa_list(oid,user_id,create_time,renew_time,status,type)
-    VALUES ($oid,'$user_name','$date','$date',0,1)
-    ON DUPLICATE KEY UPDATE 
-    renew_time='$date', status = 0,type = 1";
-    mysqli_query($connect, $insertSQL_QA_list);  
-
-
+        VALUES ($oid,'$qa_content','$filepath','$user_name','$date',1);
+    INSERT INTO qa_list(oid,user_id,create_time,renew_time,status,type)
+        VALUES ($oid,'$user_name','$date','$date',0,1)
+        ON DUPLICATE KEY UPDATE 
+        renew_time='$date', status = 0,type = 1;";
+    $SQL_query = $connect->multi_query($insertSQL_Customer_qa);
  
-   
     $result['status'] = true;
     return $result;   
 }
